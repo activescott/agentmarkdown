ee2d6adafc80b02c8994502858ba3de9c9b1f276
Scott Willeke <scott@willeke.com>
2019-07-20 01:29:34 -0700

    needs reviewed

--- src/cli/agent-markdown.spec.ts
+++ src/cli/agent-markdown.spec.ts
@@ -1,58 +1,85 @@
-import { EventEmitter } from "events"
-import {
-  exec as _exec,
-  spawn,
-  ChildProcessWithoutNullStreams
-} from "child_process"
+import { Readable, Writable } from "stream"
 import path from "path"
-import util from "util"
+import { Cli, CliProcess } from "./agent-markdown"
+import { StringDecoder } from "string_decoder"
 
-const exec = util.promisify(_exec)
-
-describe.only("CLI", () => {
-  const tsNodePath: string = path.join(
-    __dirname,
-    "../../node_modules/.bin/ts-node"
-  )
-  const tsNodeOptions: string[] = [
-    "--compiler-options",
-    `"{ \\"downlevelIteration\\": true }"`
-  ]
-  const tsNodePathAndOptions: string =
-    tsNodePath + " " + tsNodeOptions.join(" ")
-  const binScriptPath: string = path.join(__dirname, "agent-markdown.ts")
-  const exePath = tsNodePathAndOptions + " " + binScriptPath
-
-  it("should accept stdin", async () => {
-    const proc: ChildProcessWithoutNullStreams = spawn(
-      tsNodePath,
-      tsNodeOptions.concat([binScriptPath]),
-      {}
-    )
-    let markdown = ""
-    proc.stdout.on("data", data => {
-      markdown += data
-    })
-
-    proc.stdin.write("<em>emphasis</em>")
-    proc.stdin.end()
-
-    const exitResult = await EventEmitter.once(proc, "exit")
-    console.log("exitResult:", exitResult)
-
-    expect(markdown).toEqual("*emphasis*")
+describe("CLI", () => {
+  /**
+   * Represents the following command:
+   * ./node_modules/.bin/ts-node --compiler-options "{ \"downlevelIteration\": true }" src/cli/agent-markdown.ts
+   */
+  it("should show error without options", async () => {
+    const mockStdOut: MockWritable = new MockWritable()
+    const mockProcess: CliProcess = {
+      exit: jest.fn(),
+      argv: ["/usr/local/bin/node", "file.js"],
+      stdin: new MockReadable(null, true),
+      stdout: mockStdOut
+    }
+    await new Cli().run(mockProcess)
+    expect(mockStdOut.data).toMatch(/^Usage: agent-markdown \[filePath\]/)
   })
 
+  /**
+   * represents the following command:
+   * ./node_modules/.bin/ts-node --compiler-options "{ \"downlevelIteration\": true }" src/cli/agent-markdown.ts test-data/emphasis.fragment.html
+   */
   it("should process filename on arg", async () => {
     const contentPath: string = path.join(
       __dirname,
       "../../test-data/emphasis.fragment.html"
     )
-    const result: { stdout: string; stderr: string } = await exec(
-      exePath + " " + contentPath,
-      { timeout: 5000 }
-    )
+    const mockStdOut: MockWritable = new MockWritable()
+    const mockProcess: CliProcess = {
+      exit: jest.fn(),
+      argv: ["/usr/local/bin/node", "file.js", contentPath],
+      stdin: new MockReadable(null, true),
+      stdout: mockStdOut
+    }
+    await new Cli().run(mockProcess)
+    expect(mockStdOut.data).toEqual("*emphasis*")
+  })
 
-    expect(result.stdout).toEqual("*emphasis*")
+  /**
+   * Represents the following command:
+   * echo "<em>emphasis</em>" | ./node_modules/.bin/ts-node --compiler-options "{ \"downlevelIteration\": true }" src/cli/agent-markdown.ts
+   */
+  it("should accept stdin", async () => {
+    const mockStdOut: MockWritable = new MockWritable()
+    const mockProcess: CliProcess = {
+      exit: jest.fn(),
+      argv: ["/usr/local/bin/node", "file.js"],
+      stdin: new MockReadable("<em>emphasis</em>", false),
+      stdout: mockStdOut
+    }
+    await new Cli().run(mockProcess)
+    expect(mockStdOut.data).toEqual("*emphasis*")
   })
 })
+
+class MockWritable extends Writable {
+  public data = ""
+  private _decoder = new StringDecoder()
+  public constructor() {
+    super()
+  }
+  public _write(
+    chunk: string | Buffer,
+    encoding: string,
+    callback: (error?: Error | null) => void
+  ): void {
+    if (encoding === "buffer") {
+      chunk = this._decoder.write(chunk as Buffer)
+    }
+    this.data += chunk
+    callback()
+  }
+}
+
+class MockReadable extends Readable {
+  public constructor(public _data: string, public isTTY: boolean) {
+    super()
+    _data && this.push(_data)
+    this.push(null)
+  }
+}
--- src/cli/agent-markdown.ts
+++ src/cli/agent-markdown.ts
@@ -1,20 +1,43 @@
 #!/usr/bin/env node
-import { EventEmitter } from "events"
 import { AgentMarkdown } from "../"
 import { promises as fs } from "fs"
 
 const EXIT_SUCCESS = 0
+const EXIT_ERR_USAGE = 1
 const EXIT_ERR_CONVERTING = 2
 const EXIT_ERR_STDOUT = 3
 
+export interface CliProcess {
+  argv: string[]
+  stdin: StdInStream
+  stdout: StdOutStream
+  exit(code?: number): void
+}
+
+export interface StdOutStream {
+  write(data: string): void
+  end(): void
+}
+
+export interface StdInStream {
+  isTTY?: boolean
+  on(eventName: string, callback: () => void): this
+  setEncoding(encoding: string): void
+  read(size?: number): string | Buffer
+}
+
 export class Cli {
-  public async run(): Promise<void> {
-    ;(await this.processArgs()) ||
-      (await this.processStdin()) ||
-      this.showHelp()
+  public async run(process: CliProcess): Promise<void> {
+    ;(await this.processArgs(process)) ||
+      (await this.processStdin(process)) ||
+      this.showHelp(process, EXIT_ERR_USAGE)
+    process.exit(EXIT_SUCCESS)
   }
 
-  private async processHtml(html: string): Promise<boolean> {
+  private async processHtml(
+    process: CliProcess,
+    html: string
+  ): Promise<boolean> {
     let markdown = ""
     try {
       markdown = await AgentMarkdown.produce(html)
@@ -25,6 +48,7 @@ export class Cli {
     }
     try {
       process.stdout.write(markdown)
+      process.stdout.end()
     } catch (err) {
       console.error("Error writing to stdout.")
       process.exit(EXIT_ERR_STDOUT)
@@ -33,36 +57,40 @@ export class Cli {
     return true
   }
 
-  private async processStdin(): Promise<boolean> {
-    let stdinContent = ""
-    process.stdin.on("readable", () => {
-      process.stdin.setEncoding("utf8")
-      let chunk: string
-      while ((chunk = process.stdin.read() as string) !== null) {
-        stdinContent += chunk
-      }
-    })
-
-    await EventEmitter.once(process.stdin, "end")
-    if (stdinContent) {
-      return this.processHtml(stdinContent)
-    } else {
+  private async processStdin(process: CliProcess): Promise<boolean> {
+    if (process.stdin.isTTY === true) {
       return false
+    } else {
+      return new Promise(resolve => {
+        let stdinContent: string = null
+        // See example at https://nodejs.org/api/process.html#process_a_note_on_process_i_o for this:
+        process.stdin.on("readable", () => {
+          process.stdin.setEncoding("utf8")
+          stdinContent = process.stdin.read() as string
+          if (stdinContent === null) {
+            // no data was piped in
+            resolve(false)
+          } else {
+            resolve(this.processHtml(process, stdinContent))
+          }
+        })
+      })
     }
   }
 
-  private async processArgs(): Promise<boolean> {
-    const args = process.argv.slice(2)
+  private async processArgs(process: CliProcess): Promise<boolean> {
+    const DEFAULT_ARGS = 2
+    const args = process.argv.slice(DEFAULT_ARGS)
     if (args.length > 0) {
       const filePath = args[0]
       const html: string = await fs.readFile(filePath, { encoding: "utf8" })
-      return this.processHtml(html)
+      return this.processHtml(process, html)
     } else {
       return false
     }
   }
 
-  private showHelp(exitCode: number = 1): void {
+  private showHelp(process: CliProcess, exitCode: number = EXIT_SUCCESS): void {
     const help = `Usage: agent-markdown [filePath]
 
 An application to convert HTML to markdown.
@@ -71,9 +99,12 @@ An application to convert HTML to markdown.
 
 If no file arguments are specified, the standard input is used.
 `
-    console.log(help)
+    process.stdout.write(help)
+    process.stdout.end()
     process.exit(exitCode)
   }
 }
 
-new Cli().run()
+if (require.main === module) {
+  new Cli().run(process)
+}
