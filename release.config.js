d42e27c9962c7a8c18932fa7b54bc9d83ed75333
Scott Willeke <scott@willeke.com>
2019-08-20 18:02:43 -0700

    build: debugging semantic release fail w/ github actions4

---
+++ release.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // https://github.com/semantic-release/semantic-release/blob/beta/docs/usage/configuration.md
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "master",
+    "next",
+    "next-major",
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+    { name: "build-semantic-release-fix", prerelease: true }
+  ]
+}
