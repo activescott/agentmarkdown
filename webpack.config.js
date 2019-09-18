6f5f8f4ad3516a54cffb4d94fd02b0db104308c0
activescott <activescott@users.noreply.github.com>
2019-09-18 04:04:30 +0000

    Automated deployment: Wed Sep 18 04:04:30 UTC 2019 e6b6d83db503c96592f3c51762499479dce26e78

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require("path")
+
+module.exports = {
+  // https://webpack.js.org/configuration/output/
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "main.js"
+  },
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8080
+  }
+}
