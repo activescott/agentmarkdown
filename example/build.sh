d2f37e8f25722b506a0c487afa0fb00c6f86895f
Scott Willeke <scott@willeke.com>
2019-09-17 22:56:37 -0700

    docs(example): example shows commit sha rather than version of package

--- example/build.sh
+++ example/build.sh
@@ -13,6 +13,11 @@ yarn build
 # now that package is built, return to example dir and build example
 cd $THISDIR
 echo "current dir should be example dir. Is: $PWD"
+# first replace a placeholder in the example file with the current commit value:
+if [ -n "$NOW_GITHUB_COMMIT_SHA" ]; then
+  sed -i -e "s/COMMIT_SHA/$NOW_GITHUB_COMMIT_SHA/g" ./index.html
+fi
+# now build the example:
 yarn
 yarn build
 
