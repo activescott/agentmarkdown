88211bb19b2462e0df034bbaf4ab3f010c1edf00
Scott Willeke <scott@willeke.com>
2021-09-18 21:00:30 -0700

    chore: post-install script dynamically pulls info from package

--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -8,19 +8,23 @@ const BG_BLACK = "\x1b[40m"
 
 const RESET = "\x1b[0m"
 
+const package = require("../package.json")
+
 function info(msg, url) {
   process.stdout.write(
-    `${BG_BLACK}${FG_WHITE}AgentMarkdown${RESET} ${BG_BLACK}${FG_BLUE}notice${RESET} ${msg}`
+    `${BG_BLACK}${FG_WHITE}${package.name}${RESET} ${BG_BLACK}${FG_BLUE}notice${RESET} ${msg}`
   )
-  url && process.stdout.write(` ${UNDERLINE}${url}${RESET}`)
+  // NOTE: During install npm will update a `repository: "https://github.com/..."` to be an object with a url property.
+  url = url && url.url ? url.url : url
+  url && process.stdout.write(`${UNDERLINE}${url}${RESET}`)
   process.stdout.write(EOL)
 }
 
 info(
-  "Did AgentMarkdown help you? Please add a ⭐ ",
-  "https://github.com/activescott/agentmarkdown"
+  `Did ${package.name} help you? Please add a ⭐ at `,
+  package.repository
 )
 info(
-  "Not helpful? Please share your feedback 📣 ",
-  "https://github.com/activescott/agentmarkdown/issues"
+  "Not helpful? Please share your feedback 📣 at ",
+  package.bugs.url
 )
