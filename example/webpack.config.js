2d54773c8f67407c173ba5876da89ce0b32996c8
Scott Willeke <scott@willeke.com>
2022-07-20 19:22:40 -0700

    chore(deps): migrate webpack from v4 to v5

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,14 +1,23 @@
 var path = require("path")
 
 module.exports = {
+  mode: "production",
   // https://webpack.js.org/configuration/output/
   output: {
     path: path.join(__dirname, "dist"),
     filename: "main.js",
   },
+  resolve: {
+    fallback: {
+      // webpack < 5 used to include polyfills for node.js core modules by default. This is no longer the case, so we include events fallback for browsers
+      events: require.resolve("events/"),
+    },
+  },
   // https://webpack.js.org/configuration/dev-server/
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
     compress: true,
     port: 8080,
   },
